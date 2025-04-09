990645978cf4119c401c4666985a9bcc392c7c15
morizz-06 <Pauloroberto6678@gmail.com>
2025-04-09 15:00:54 -0300

    Add files via upload

---
+++ www/aula1/variaveis.js
@@ -0,0 +1,7 @@
+var nome = "Paulo Roberto"
+var idade = 18
+var rua= "Rua Felipe tabet N° 62"
+var bairro = " no dudu lopes"
+var cidade = " na cidade de Valença-RJ"
+
+console.log (" Meu nome é "  +  nome + " tenho " + idade + " anos."  + " Meu endereço é " +  rua +  bairro +  cidade + ".")
---
+++ www/aula_2/tipos_variaveis.js
@@ -0,0 +1,58 @@
+var nome = "Maria";                  // string
+console.log(typeof nome)
+
+var idade = 30;                      // number
+console.log(typeof idade)
+
+var estudando = true;                //  boolean
+console.log(typeof estudando)
+
+var sobrenome = "Silva";             // string
+console.log(typeof sobrenome)
+
+var saldo = 0;                       // number
+console.log(typeof saldo)
+
+var indefinido;                      // undefined
+console.log(typeof indefinido)
+
+var cidade = "Rio de Janeiro";       // string
+console.log(typeof cidade)
+
+var numeroGrande = 1000000;          // number
+console.log(typeof numeroGrande)
+
+
+var curso = "JavaScript";            // string
+console.log(typeof curso)
+
+var temperatura = -5;                // number
+console.log(typeof temperatura)
+
+var saudacao = "Olá, tudo bem?";     // string
+console.log(typeof saudacao)
+
+var notaProva = 8.75;                // number
+console.log(typeof notaProva )
+
+var nada = null;                     // undefined
+console.log(typeof nada )
+
+var numeroComoTexto = "123";         // string
+console.log(typeof numeroComoTexto )
+ 
+var lista = [10, 20, 30];            // object
+console.log(typeof lista )
+
+var objeto = {marca: "Nike", tamanho: 42}; //object
+console.log(typeof objeto )
+
+var anoNascimento = 1993;             // number
+console.log(typeof anoNascimento )
+  
+var vazio = "";                       // string
+console.log(typeof vazio )
+
+var altura = 1.65;                    // number
+console.log(typeof altura )
+   
