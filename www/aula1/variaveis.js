7699285e01bcef85327c82ed147c95e902fbf5fe
morizz-06 <Pauloroberto6678@gmail.com>
2025-04-09 15:07:25 -0300

    Update variaveis.js

--- www/aula1/variaveis.js
+++ www/aula1/variaveis.js
@@ -1,7 +1,7 @@
-var nome = "Paulo Roberto"
-var idade = 18
-var rua= "Rua Felipe tabet N° 62"
-var bairro = " no dudu lopes"
+var nome = "felipe ferreira"
+var idade = 32
+var rua= "Rua luciomeira campos"
+var bairro = " vaginha"
 var cidade = " na cidade de Valença-RJ"
 
 console.log (" Meu nome é "  +  nome + " tenho " + idade + " anos."  + " Meu endereço é " +  rua +  bairro +  cidade + ".")
